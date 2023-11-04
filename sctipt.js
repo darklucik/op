9fb594d1193d553155da8538d03ec782a3422019
darklucik <rustamzonovsultan3@gmail.com>
2023-11-04 22:40:25 +0500

    init

---
+++ sctipt.js
@@ -0,0 +1,24 @@
+
+
+function codeOper() {
+    let code = Number(document.getElementById("codeNum").value)
+
+    if(code==97 || code==88) {
+        document.getElementById("compOper").innerHTML = "MobiUz"
+    }
+    if(code==90 || code==91) {
+        document.getElementById("compOper").innerHTML = "Beeline"
+    }
+    if(code==93 || code==94 || code==55) {
+        document.getElementById("compOper").innerHTML = "Ucell"
+    }
+    if(code==33) {
+        document.getElementById("compOper").innerHTML = "Humans"
+    }
+    if(code==95) {
+        document.getElementById("compOper").innerHTML = "Uzmobile"
+    }
+    if(code==99 || code==77) {
+        document.getElementById("compOper").innerHTML = "Uzmobile GSM"
+    }
+}
